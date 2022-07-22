ede6f9103eb190d01d653798f7d90c37874a576c
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-07-21 19:37:04 -0700

    Fix grant for monitoring schema.

--- 6_grants_monitoring_schema.tf
+++ 6_grants_monitoring_schema.tf
@@ -12,18 +12,6 @@ resource "snowflake_schema_grant" "snowalert_monitoring_schema_grant_create_task
   with_grant_option = false
 }
 
-resource "snowflake_schema_grant" "snowalert_monitoring_schema_grant_ownership" {
-  provider = snowflake.security_admin_role
-
-  database_name = var.snowalert_database_name
-  schema_name   = var.monitoring_schema_name
-
-  privilege = "OWNERSHIP"
-  roles = [
-    var.security_monitoring_role,
-  ]
-}
-
 resource "snowflake_schema_grant" "snowalert_monitoring_schema_grant_create_function" {
   provider = snowflake.security_admin_role
 
