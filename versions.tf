53cbe6e36eda1e3c9615b47e4f3523aa91a724d5
Prasanth Kommini <prasanth.kommini@snowflake.com>
2022-11-04 22:31:50 -0700

    Bump version.

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.49.0"
+      version = "~> 0.48.0"
 
       configuration_aliases = [
         snowflake.security_admin_role,
